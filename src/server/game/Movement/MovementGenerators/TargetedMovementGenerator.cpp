8cd81780acc99e4093dafa0565cad4a687663070
Cristian Bilu <cristian.bilu@ipsos.com>
2021-01-20 03:12:04 +0200

    Revert "improve initial positioning"
    This reverts commit 31256c606e5c9b3038d984e83dfd1fe9f26aa821.

--- src/server/game/Movement/MovementGenerators/TargetedMovementGenerator.cpp
+++ src/server/game/Movement/MovementGenerators/TargetedMovementGenerator.cpp
@@ -76,13 +76,7 @@ void TargetedMovementGeneratorMedium<T, D>::_setTargetLocation(T* owner, bool in
         }
 
         // to nearest contact position
-        /** NOTE: creatures should reach you using the
-        * nearest contact point, however, disabling it
-        * simplify the job for the repositioning algorithm
-        * that must be still used to resolve creature collisions
-        * @todo we could probably create a conf for it.
-        */
-        //i_target->GetContactPoint(owner, x, y, z);
+        i_target->GetContactPoint(owner, x, y, z);
     }
     else
     {
